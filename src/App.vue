8bbc24fe009f2ab66feb4141041bbf160f28e2ba
nam.dao <nam.dao@kobizo.de>
2022-07-31 16:12:30 +0700

    Config

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>Hello con meap</div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    darkMode: 'media',
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
